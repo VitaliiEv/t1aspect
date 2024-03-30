fc6988194cb717ffd57597d1774c989a2d01a037
Vitalii Solomonov <solomonov.v@gmail.com>
2024-03-30 13:48:09 +0300

    feat: init

---
+++ src/main/java/com/github/vitaliiev/t1aspect/T1aspectApplication.java
@@ -0,0 +1,13 @@
+package com.github.vitaliiev.t1aspect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1aspectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1aspectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/vitaliiev/t1aspect/T1aspectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vitaliiev.t1aspect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1aspectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
